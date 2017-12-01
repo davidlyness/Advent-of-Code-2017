19f65bd94d992f642ed081bd8f0e4a0b132bf0a5
David Lyness <davidlyness@users.noreply.github.com>
2017-12-01 10:09:24 +0000

    Make less verbose using Python list comprehensions

--- 1/1a.py
+++ 1/1a.py
@@ -4,9 +4,4 @@
 with open("input.txt") as f:
     digits = f.read().rstrip("\n")
 
-digit_sum = 0
-for i in range(len(digits)):
-    if digits[i] == digits[(i+1) % len(digits)]:
-        digit_sum += int(digits[i])
-
-print(digit_sum)
+print(sum([int(digits[i]) for i in range(len(digits)) if digits[i] == digits[(i+1) % len(digits)]]))
