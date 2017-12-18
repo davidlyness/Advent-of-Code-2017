824b84eb1139ecc19bf9d0da8dcca38512eb7cf0
David Lyness <davidlyness@users.noreply.github.com>
2017-12-18 10:06:34 +0000

    Add solution for Day 18

---
+++ 18/18a.py
@@ -0,0 +1,56 @@
+# coding=utf-8
+"""Advent of Code 2017, Day 18. Part 1"""
+
+with open("input.txt") as f:
+    instructions = [x.split(" ") for x in f.read().rstrip("\n").split("\n")]
+registers = dict.fromkeys([instruction[1] for instruction in instructions if instruction[1].isalpha()], 0)
+
+
+def get_value(input_value):
+    """
+    Determine the register or numeric value of the input.
+    :param input_value: register or number to inspect
+    :return: value of register or number
+    """
+    if input_value.isalpha():
+        return registers[input_value]
+    else:
+        return int(input_value)
+
+
+def run_program():
+    """
+    Run program up to the first valid `rcv` instruction.
+    :return: value emitted by the first valid `rcv` instruction
+    """
+    i = 0
+    current_frequency = None
+    while 0 <= i < len(instructions):
+        instruction = instructions[i]
+        if instruction[0] == "snd":
+            current_frequency = get_value(instruction[1])
+            i += 1
+        elif instruction[0] == "set":
+            registers[instruction[1]] = get_value(instruction[2])
+            i += 1
+        elif instruction[0] == "add":
+            registers[instruction[1]] += get_value(instruction[2])
+            i += 1
+        elif instruction[0] == "mul":
+            registers[instruction[1]] *= get_value(instruction[2])
+            i += 1
+        elif instruction[0] == "mod":
+            registers[instruction[1]] %= get_value(instruction[2])
+            i += 1
+        elif instruction[0] == "rcv":
+            if get_value(instruction[1]) != 0:
+                return current_frequency
+            i += 1
+        elif instruction[0] == "jgz":
+            if get_value(instruction[1]) > 0:
+                i += get_value(instruction[2])
+            else:
+                i += 1
+
+
+print(run_program())
---
+++ 18/18b.py
@@ -0,0 +1,99 @@
+# coding=utf-8
+"""Advent of Code 2017, Day 18. Part 1"""
+
+with open("input.txt") as f:
+    instructions = [x.split(" ") for x in f.read().rstrip("\n").split("\n")]
+
+
+class Program:
+    """
+    An instance of a Program.
+    """
+
+    def __init__(self, program_number):
+        self.registers = dict.fromkeys([instruction[1] for instruction in instructions if instruction[1].isalpha()], 0)
+        self.registers['p'] = program_number
+        self.instruction_index = 0
+        self.message_queue = []
+        self.is_waiting = False
+        self.num_values_sent = 0
+
+    def add_message_to_queue(self, message):
+        """
+        Add a message to the program's message queue.
+        :param message: message to add to the queue
+        """
+        self.message_queue.append(message)
+
+    def get_value(self, input_value):
+        """
+        Determine the register or numeric value of the input.
+        :param input_value: register or number to inspect
+        :return: value of register or number
+        """
+        if input_value.isalpha():
+            return self.registers[input_value]
+        else:
+            return int(input_value)
+
+    def run_instruction(self):
+        """
+        Run a single instruction for the program.
+        :return: a message if emitted; None otherwise
+        """
+        message = None
+        instruction = instructions[self.instruction_index]
+        if instruction[0] == "snd":
+            message = self.get_value(instruction[1])
+            self.num_values_sent += 1
+            self.instruction_index += 1
+            self.is_waiting = False
+        elif instruction[0] == "set":
+            self.registers[instruction[1]] = self.get_value(instruction[2])
+            self.instruction_index += 1
+            self.is_waiting = False
+        elif instruction[0] == "add":
+            self.registers[instruction[1]] += self.get_value(instruction[2])
+            self.instruction_index += 1
+            self.is_waiting = False
+        elif instruction[0] == "mul":
+            self.registers[instruction[1]] *= self.get_value(instruction[2])
+            self.instruction_index += 1
+            self.is_waiting = False
+        elif instruction[0] == "mod":
+            self.registers[instruction[1]] %= self.get_value(instruction[2])
+            self.instruction_index += 1
+            self.is_waiting = False
+        elif instruction[0] == "rcv":
+            if len(self.message_queue) > 0:
+                self.registers[instruction[1]] = self.message_queue.pop(0)
+                self.instruction_index += 1
+                self.is_waiting = False
+            else:
+                self.is_waiting = True
+        elif instruction[0] == "jgz":
+            if self.get_value(instruction[1]) > 0:
+                self.instruction_index += self.get_value(instruction[2])
+            else:
+                self.instruction_index += 1
+            self.is_waiting = False
+        return message
+
+
+program_0 = Program(0)
+program_1 = Program(1)
+active_program = 0
+
+while not (program_0.is_waiting and program_1.is_waiting):
+    if active_program == 0:
+        program_message = program_0.run_instruction()
+        if program_message is not None:
+            program_1.add_message_to_queue(program_message)
+        active_program = 1
+    else:
+        program_message = program_1.run_instruction()
+        if program_message is not None:
+            program_0.add_message_to_queue(program_message)
+        active_program = 0
+
+print(program_1.num_values_sent)
